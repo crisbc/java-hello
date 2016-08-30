6fd594b8b5156669195d9609a94fa0b06c158bbf
Cristina <Cristina@65.126.171.131>
2016-08-30 12:09:32 -0600

    basic hello version

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import static kiss.API.*;
+/**
+ *
+ * @author Cristina
+ */
+public class Helloworld {
+    void testRun()
+    {
+        outExpect("Hello!");
+        run();
+        outClose();
+    }
+
+    void run()
+    {
+        println("Hello!");
+    }
+}
