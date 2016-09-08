654cf2e346c2b9595173d7c38ae94a287064c4ae
Cristina <Cristina@65.126.171.131>
2016-09-08 12:30:01 -0600

    with ints function pero unos no sirven

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -17,12 +17,106 @@ public class Helloworld {
         run();
         outClose();
     }
-    void testAdd()
-    {
-        assert 1+1 == 2; 
-    }
+
     void run()
     {
         println("Hello!");
+        
+        //println("How are you?");
+        
+    }
+  
+    void sideEffect(int y)
+    {
+        println(y);
+    }
+    void testInts()
+    {
+        assert Integer.MAX_VALUE == Math.pow(2,31)-1;
+        println(Integer.MAX_VALUE);
+       // assert Integer.MAX_VALUE == 0b1000_0000_0000_0000_0000_0000_0000_0000;
+        
+        int x =3;
+        int y = 0xff__ff;
+        int z = 0b10_01;
+        
+        assert x == 3;
+        assert y == 65_535;
+        assert z ==9;
+        
+        assert Integer.toString(x,10).equals ("3");
+        assert Integer.toString(y, 16).equals("ffff");
+        assert Integer.toString(z, 2).equals("1001");
+        
+        
+       // assert Integer.MIN_VALUE == 
+                
+                
+       //mero abajo de ints
+  int a =1;
+  //cala los incrementors
+  //add antes
+  ++a;
+     try (Close out = outExpect ("2")) { println(a);}
+     try (Close out = outExpect ("3")) { println(++a);}
+     try (Close out = outExpect ("3")) { println(a);}
+     
+     //add depues
+    // try(Close out = outExpect ("3")) { println(a++);}
+     //try(Close out = outExpect ("3")) { println(a);}
+     
+     //bitwise not
+    assert ~0b0000_0000_0000_0000_0000_0000_1111_0000
+          == 0b0000_1111_1111_1111_1111_1111_0000_1111;
+     
+     //bitwise and
+     assert (0b1111_0000 
+             & 0b1010_1010) ==
+             0b1010_0000;
+     
+     //bitwise or
+     assert (0b1111_0000 
+             | 0b1010_1010) ==
+             0b1111_1010;
+     
+     //bit shift operator right
+     
+     assert (0b1010_1111_0000 >> 4) ==
+             0b1010_1111;
+     //bit shift left
+     assert (0b1010_1111_0000 << 4) ==
+             0b1010_1111_0000_0000;
+     
+     //random gives a random number between 0 to 1,000,000
+     // el bit shift todavia
+     int b = random(0,1_000_000);
+     println(b);
+     
+     assert ((b >>1) == b/2);
+     assert (((-b) >> 1) == -b/2);
+     assert ((b << 1) == 2*b);
+     assert(((-b) << 1)== 2* (-b));
+     //
+     int c = random(-1_000_000,1_000_000);
+     assert ((~c)+1) == -c;
+     
+     assert -2 == 0b1111_1111_1111_1111_1111_1111_1111_1110;
+     
+     //right shifts shift the sign bit in (highest order bit)
+     assert (-2 >> 1) == 0b1111_1111_1111_1111_1111_1111_1111_1110;
+     
+     println(Integer.toString(-2, 2));
+     
+    }
+    void testConvert()
+    {
+        
+        byte x =-1;
+        assert x == (byte) 0b1111_1111;
+        int y =x;
+        assert y == -1;
+        assert 0b1111_1111_1111_1111_1111_1111_1111_1111 ==
+                               (int) (byte) 0b1111_1111;
+        
     }
 }
